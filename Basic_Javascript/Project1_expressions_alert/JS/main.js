63a469e17271e7e270cd49698f583289ba014508
ImminentCloud <peachwave1234@gmail.com>
2024-03-06 16:51:06 -0800

    Basic javascript
    Learning the basics of JavaScript

---
+++ Basic_Javascript/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+var A = "This is a string";
+document.write(A);
+window.alert(A);
+document.write("Hello it's \"thursday\""
+ + "and tomrrow is Friday");
+ var B = `Concatenated String`
+ document.write(B)
+ var Banana = 'Yellow', Grape = 'Purple',
+    Strawberry = 'Red';
+document.write(Banana);     //writing the color of the var banana
+document.write(8+9);    //Adding 8 and 9 together
